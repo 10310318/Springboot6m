e9e14611acc71114a820d3e66c0d34a00f9ee439
10310318 <doc20151008@gmail.com>
2020-06-18 15:19:57 +0800

    add lombok

---
+++ src/main/java/com/chtti/springboot/demo/Demo7JPADocker/beans/Beverage.java
@@ -0,0 +1,10 @@
+package com.chtti.springboot.demo.Demo7JPADocker.beans;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Beverage {
+    private Long id;
+    private String title;
+    private String detail;
+}
